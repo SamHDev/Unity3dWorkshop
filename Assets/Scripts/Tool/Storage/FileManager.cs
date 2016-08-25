cb170d50eec7490ccda1204e68ab19faa273b984
Sam Huddart <sam02h.huddart@gmail.com>
2016-08-25 16:01:51 +0100

    stuff

---
+++ Assets/Scripts/Tool/Storage/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class FileManager : MonoBehaviour {
+
+
+    public string getdata(string filen)
+    {
+        if (File.Exists(filen + ".txt"))
+        {
+            string lol = "";
+            StreamReader file = new StreamReader(filen + ".txt");
+            lol = file.ReadToEnd();
+            file.Close();
+            return lol;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void writedata(string filen, string txt)
+    {
+        if (File.Exists(filen + ".txt"))
+        {
+            string lol = "";
+            StreamWriter file = new StreamWriter(filen + ".txt");
+            file.Write(txt);
+            file.Close();
+        }
+        else
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/YourComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YourComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Starting App");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.LogFormat ("Time={0}", Time.time);
+	}
+}
